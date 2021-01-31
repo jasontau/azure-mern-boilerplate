7875b519794b3c829162a16703a5aea02553160c
Jason Au <jasontau@gmail.com>
2021-01-31 10:02:35 -0800

    changed mongo uri

--- routes/new-index.js
+++ routes/new-index.js
@@ -3,14 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
-
+var url = "mongodb+srv://admin:passw0rd123@dev.o6xsz.gcp.mongodb.net/posts?retryWrites=true&w=majority";
 
 router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("<database>");
-		dbo.collection("collection1").find({}).toArray(function(err, result) {
+		var dbo = db.db("posts");
+		dbo.collection("Collection0").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
     		console.log(result);
